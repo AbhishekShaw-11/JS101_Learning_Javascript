f461f47524e87acc9f57ad2a5ab7021824ff2af6
abhishek 1199 <abhishekshaw1199@gmail.com>
2022-11-04 16:29:00 +0530

    my first work

---
+++ JS-IV-C-S-IWjs/p3.js
@@ -0,0 +1,9 @@
+let a=40;
+let b=42;
+if (a>b){
+  console.log(a,"Greater!");
+} else if (b>a){
+  console.log(b,"Greater!");
+}else {
+  console.log("Both are equal")
+}
---
+++ JS-IV-C-S-IWjs/p4.js
@@ -0,0 +1,14 @@
+ let dataBase_user= "Abhi11@gmail.com";
+let dataBase_pass="abhi11";
+// for login
+let user="Abhi11@gmail.com";
+let pass="abhi11";
+if(dataBase_user==user){
+  if(dataBase_pass==pass){
+    console.log("Login  into profile");
+  }else{
+    console.log("incorrect password");
+  }
+}else{
+  console.log("Wrong credentials")
+}
